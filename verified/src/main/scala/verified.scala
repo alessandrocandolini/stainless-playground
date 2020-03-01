d39e45669af791f01990ee0f95c92a3a54e18da7
Alessandro Candolini <alessandrocandolini@gmail.com>
2020-03-01 02:05:12 +0000

    Example with nonempty string

--- verified/src/main/scala/verified.scala
+++ verified/src/main/scala/verified.scala
@@ -8,5 +8,22 @@ object verified {
     1 + 1 == 2
   }.holds
 
+  def f =
+    Positive(1)
+
+  def g : String => Option[NonEmptyString] =
+    s =>
+      Some(s).filter(s => s!="").map(s => NonEmptyString(s))
+
+}
+
+final case class Positive(value: BigInt) extends AnyVal {
+  @invariant
+  def isPositive: Boolean = value >= 0
+}
+
+final case class NonEmptyString(value: String) extends AnyVal {
+  @invariant
+  def nonEmpty: Boolean = value != ""
 }
 
